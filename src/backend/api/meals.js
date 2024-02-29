829dfcfa32a5ef6d4ca25a60247a88f289fcab65
Niloufar <nilooooofar97@gmail.com>
2024-02-29 08:58:27 +0100

    return a meal by id

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../database");
 
+// Returns all meals
 router.get("/", async (request, response) => {
   try {
     const allMeals = await knex("Meal").select();
@@ -11,24 +12,52 @@ router.get("/", async (request, response) => {
   }
 });
 
-router.post("/" , async(req, res) => {
- try{
-  const { title, description, location, _when, max_reservations, price, created_date } = req.body;
+// 	Adds a new meal to the database
+router.post("/", async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      location,
+      _when,
+      max_reservations,
+      price,
+      created_date,
+    } = req.body;
 
-  const newMeal = await knex("Meal").insert({
-    title,
-    description,
-    location,
-    _when,
-    max_reservations,
-    price,
-    created_date
-  })
-  res.status(201).json({data: newMeal[0] , message: "ok"})
- }catch(err){
-  console.error("Error adding meal:", err);
-  res.status(500).json({ error: "Failed to add meal" });
- } 
+    const newMeal = await knex("Meal").insert({
+      title,
+      description,
+      location,
+      _when,
+      max_reservations,
+      price,
+      created_date,
+    });
+    res.status(201).json({ message: "new meal added" });
+  } catch (err) {
+    console.error("Error adding meal:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
+
+// 	Returns the meal by id
+router.get("/:id", async (req, res) => {
+  try {
+    const mealId = +req.params.id;
+    const selectedMeal = await knex("Meal")
+      .where("meal_id", "=", mealId)
+      .select();
+    if (selectedMeal.length === 0) {
+      return res.status(404).json({ data: null, message: "not found" });
+    }
+    res.status(200).json({ data: selectedMeal, message: "ok" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
 
 module.exports = router;
