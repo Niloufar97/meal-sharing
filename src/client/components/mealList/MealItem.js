0c2bf1f6c7105d0f77affddb67c0416b1cbd66cd
Niloufar <nilooooofar97@gmail.com>
2024-04-03 17:04:56 +0200

    add meal Item

---
+++ src/client/components/mealList/MealItem.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from './meal.module.css'
+
+function MealItem({meal}) {
+  return (
+    <div className={styles["meal-card"]}>
+      <img src={meal.img} alt={meal.title}/>
+      <h2>{meal.title}</h2>
+      <p>{meal.description}</p>
+      <h6>Price: {meal.price}</h6>
+    </div>
+  )
+}
+
+export default MealItem
--- src/client/components/mealList/MealList.js
+++ src/client/components/mealList/MealList.js
@@ -1,9 +1,12 @@
 import useSWR from "swr";
 import React from "react";
+import MealItem from "./MealItem.js";
+import styles from './meal.module.css'
 
 const fetcher = (url) => {
   return fetch(url).then((res) => res.json());
 };
+
 
 function MealList() {
   const { data, error } = useSWR("http://localhost:5000/all-meals", fetcher);
@@ -11,9 +14,9 @@ function MealList() {
   if (error) return <div>Error in fetching data</div>;
   
   return (
-    <div>
+    <div className={styles["meal-container"]}>
       {data.map((meal) => (
-        <p key={meal.id}>{meal.title}</p>
+        <MealItem key={meal.meal_id} meal={meal} />
       ))}
     </div>
   );
