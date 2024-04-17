c452b33b69bd2e0cf5901c9d0a4630bb44cc5ac6
Niloufar <nilooooofar97@gmail.com>
2024-04-17 09:47:10 +0200

    add top meals route

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,76 +2,113 @@ import express from "express";
 const router = express.Router();
 import knex from "../database.js";
 import joi from "joi";
-import {getMealsUnderMaxPrice ,getAvailableReservations, getMealWithTitle, getMealAfterDate, getMealBeforeDate, getLimitedMeals, getSortedMeals } from './controllers/mealsControllers.js'
+import {
+  getMealsUnderMaxPrice,
+  getAvailableReservations,
+  getMealWithTitle,
+  getMealAfterDate,
+  getMealBeforeDate,
+  getLimitedMeals,
+  getSortedMeals,
+} from "./controllers/mealsControllers.js";
 
 const MealSchema = joi.object({
-  title : joi.string().required(),
+  title: joi.string().required(),
   description: joi.string(),
   location: joi.string().required(),
-  _when : joi.date().iso().required(),
+  _when: joi.date().iso().required(),
   max_reservations: joi.number().integer().min(1).required(),
   price: joi.number().min(0).required(),
-  created_date : joi.date().iso()
+  created_date: joi.date().iso(),
 });
 
 // Returns all meals
 router.get("/", async (req, res) => {
-  const {maxPrice , availableReservations, title, dateAfter, dateBefore, limit, sortKey, sortDir} = req.query
-  
+  const {
+    maxPrice,
+    availableReservations,
+    title,
+    dateAfter,
+    dateBefore,
+    limit,
+    sortKey,
+    sortDir,
+  } = req.query;
+
   try {
     let response = {
       data: [],
       status: 200,
       message: "ok",
     };
-   
-    if(maxPrice) await getMealsUnderMaxPrice(maxPrice, response);
-    
-    if(availableReservations) await getAvailableReservations(availableReservations, response);
 
-    if(title) await getMealWithTitle(title, response);
+    if (maxPrice) await getMealsUnderMaxPrice(maxPrice, response);
 
-    if(dateAfter) await getMealAfterDate(dateAfter, response);
+    if (availableReservations)
+      await getAvailableReservations(availableReservations, response);
 
-    if(dateBefore) await getMealBeforeDate(dateBefore , response);
+    if (title) await getMealWithTitle(title, response);
 
-    if(limit) await getLimitedMeals(limit, response);
+    if (dateAfter) await getMealAfterDate(dateAfter, response);
 
-    if(sortKey) await getSortedMeals(sortKey, sortDir, response);
+    if (dateBefore) await getMealBeforeDate(dateBefore, response);
 
-    if(Object.keys(req.query).length === 0){
-      const allMeals = await knex('Meal').select('*')
-      response.data = allMeals
+    if (limit) await getLimitedMeals(limit, response);
+
+    if (sortKey) await getSortedMeals(sortKey, sortDir, response);
+
+    if (Object.keys(req.query).length === 0) {
+      const allMeals = await knex("Meal").select("*");
+      response.data = allMeals;
     }
 
-    res.status(response.status).json({data : response.data , message : response.message})
+    res
+      .status(response.status)
+      .json({ data: response.data, message: response.message });
   } catch (error) {
     throw error;
   }
 });
 
-router.get("/:id/reviews" , async(req,res)=>{
+router.get("/:id/reviews", async (req, res) => {
   const mealId = +req.params.id;
   const reviwesForMeal = await knex("Review")
-    .select('Review.title', 'Review.description', 'Meal.meal_id')
-    .join('Meal' , 'Meal.meal_id' , '=' , 'Review.meal_id')
-    .where('Meal.meal_id', '=' , mealId)
+    .select("Review.title", "Review.description", "Meal.meal_id")
+    .join("Meal", "Meal.meal_id", "=", "Review.meal_id")
+    .where("Meal.meal_id", "=", mealId);
 
-  res.status(200).json({data:reviwesForMeal , message: "ok"})
+  res.status(200).json({ data: reviwesForMeal, message: "ok" });
+});
+
+router.get("/bestsellers", async (req, res) => {
+  try {
+    const bestsellers = await knex("Meal")
+      .select("Meal.meal_id", "Meal.title", "Meal.description" , "Meal.img")
+      .avg("Review.stars as Average_Stars")
+      .join("Review", "Meal.meal_id", "=", "Review.meal_id")
+      .groupBy("Meal.meal_id")
+      .orderBy("Average_Stars", "desc")
+      .limit(3);
+
+    res.status(200).json({ data: bestsellers, message: "Top meals based on average stars" });
+  } catch (error) {
+    console.error('Error fetching bestseller meals:', error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 // 	Adds a new meal to the database
 router.post("/", async (req, res) => {
   try {
-    const {error} = MealSchema.validate(req.body, { abortEarly: false });
-    if(error){
-      const errorMessages = error.details.map(detail => detail.message);
+    const { error } = MealSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      const errorMessages = error.details.map((detail) => detail.message);
       return res.status(400).json({ errors: errorMessages });
     }
 
     await knex("Meal").insert(req.body);
 
-    res.status(201).json({message: "new meal added successfully" });
+    res.status(201).json({ message: "new meal added successfully" });
   } catch (err) {
     console.error("Error adding meal:", err);
     res.status(500).json({ error: "Internal server error" });
@@ -95,20 +132,20 @@ router.get("/:id", async (req, res) => {
 // Updates the meal by id
 router.put("/:id", async (req, res) => {
   try {
-    const {error} = MealSchema.validate(req.body , {abortEarly : false})
-    if(error){
-      const errorMessages = error.details.map(detail => detail.message);
-      return res.status(400).json({errors : errorMessages})
+    const { error } = MealSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      const errorMessages = error.details.map((detail) => detail.message);
+      return res.status(400).json({ errors: errorMessages });
     }
     const mealId = +req.params.id;
 
     const mealToUpdate = await knex("Meal")
       .where("meal_id", "=", mealId)
       .update(req.body);
-    if(!mealToUpdate){
-      return res.json({message : "There is no meal with this Id"})
+    if (!mealToUpdate) {
+      return res.json({ message: "There is no meal with this Id" });
     }
-    res.status(200).json({ message: "meal updated"})
+    res.status(200).json({ message: "meal updated" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
@@ -116,17 +153,16 @@ router.put("/:id", async (req, res) => {
 });
 
 // Deletes the meal by id
-router.delete("/:id" , async(req, res) => {
-  try{
+router.delete("/:id", async (req, res) => {
+  try {
     const mealId = +req.params.id;
-    const mealToDelete = await knex("Meal").where("meal_id" , "=" , mealId).del();
+    const mealToDelete = await knex("Meal").where("meal_id", "=", mealId).del();
 
-    if(!mealToDelete){
-      return res.json({message : "There is no meal with this Id"})
+    if (!mealToDelete) {
+      return res.json({ message: "There is no meal with this Id" });
     }
-    res.status(200).json({message : "Meal deleted successfully"});
-    
-  }catch (err) {
+    res.status(200).json({ message: "Meal deleted successfully" });
+  } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
