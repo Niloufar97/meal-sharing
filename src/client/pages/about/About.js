d9cbb12bd4438cba25522ec601c4196314362b00
Niloufar <nilooooofar97@gmail.com>
2024-04-28 13:10:17 +0200

    add about text

--- src/client/pages/about/About.js
+++ src/client/pages/about/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1>this is about page</h1>
+      <p>Have you ever craved a delicious home-cooked meal but lacked the time or inspiration to cook yourself? Or perhaps you're a culinary whiz with a signature dish you'd love to share? Welcome to MealSharing, the online marketplace that connects passionate home cooks with eager eaters in your community. Here, you can browse a vibrant selection of homemade meals, discover hidden culinary gems, and savor the taste of home with every bite. Want to be a part of the action? </p>
     </div>
   )
 }
